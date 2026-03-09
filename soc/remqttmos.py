2f8c6c1060dea94edc75804720398a513aac22f2
Eswar-tech <eswaraprasath74@gmail.com>
2026-03-09 12:45:26 +0530

    Update remqttmos.py

--- soc/remqttmos.py
+++ soc/remqttmos.py
@@ -10,6 +10,7 @@ client.on_message = on_message
 
 # connect to broker on same machine
 client.connect("localhost",1883)
+#if the mosquito is installed in the subscriper and if you install mosquito in the publisher you want to specf the address of the subscriber 
 
 client.subscribe("v1/temperature")
 
